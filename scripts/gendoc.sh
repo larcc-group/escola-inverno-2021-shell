abe88d6f3c0310d11de1817c68130caab15de857
rickleon <ricardo.leonarczyk95@gmail.com>
2021-07-25 22:52:33 -0300

    Ajusta a saída do gendoc para ficar de acordo com os padrões HTML.

--- scripts/gendoc.sh
+++ scripts/gendoc.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 cat << WRAP-HTML
-<html>
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
 <title>Referência de comandos</title>
+<meta charset="UTF-8">
+<meta name="description" content="Uma referência rápida de comandos para auxiliar no aprendizado do Bash.">
+<meta name="keywords" content="Bash, tldr, reference">
 </head>
 <body>
 WRAP-HTML
